e4207f47e177ad1bd0697a5c51c143624d51de42
Jeremy Delley <jeremy.delley@imex-dtp.com>
2024-12-02 10:36:49 +0100

    commit before bump of embassy-net

--- build.rs
+++ build.rs
@@ -5,11 +5,12 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
 
     // println!("cargo:rustc-link-arg-bins=-Trom_functions.x");
+    dotenvy::dotenv().unwrap();
     wifi_secret();
+    broadcast_options();
 }
 
 fn wifi_secret() {
-    dotenvy::dotenv().unwrap();
     let ssid = env::var("SSID").unwrap();
     let passw = env::var("PASSWORD").unwrap();
     let dest_path = Path::new("src/").join("wifi_secret.rs");
@@ -17,4 +18,11 @@ fn wifi_secret() {
 
     writeln!(f, "pub const SSID: & str = \"{}\";", ssid).unwrap();
     writeln!(f, "pub const PASSWORD: & str = \"{}\";", passw).unwrap();
+}
+
+fn broadcast_options() {
+    let current = env::var("DEVICE_NAME").unwrap();
+    let dest_path = Path::new("src/").join("broadcast_options.rs");
+    let mut f = File::create(dest_path).unwrap();
+    writeln!(f, "pub const CURRENT_DEVICE_NAME: &str = \"{}\";", current).unwrap();
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # WIP: MQTT Client with Screen on ESP32 microcontroller
+
+It's like a bipper, but shitier
+-N.Y. Times
+
+If you don't hear me, I will scream louder
+-Mom
+
+Why don't you buy something that already work?
+-Sister
